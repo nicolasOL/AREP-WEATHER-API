9999456e413dfcb339d0bbdea1d956184eb370b5
Nicolas Ortega <nicolas.ortega-l@mail.escuelaing.edu.co>
2021-02-17 14:41:14 -0500

    Full Tests

--- src/test/java/edu/escuelaing/arem/clima/mainTest.java
+++ src/test/java/edu/escuelaing/arem/clima/mainTest.java
@@ -73,5 +73,46 @@ public class mainTest {
         }
 
     }
+    
+    /**
+     * Test of consultWeather method at the Heroku deployment for Bogota
+     */
+    @Test
+    public void testConsultWeatherBogotaHeroku() {
+        try {
+            System.out.println("consultWeather");
+            String expResult="{\"coord\":{\"lon\":-0.1257,\"lat\":51.5085},\"weather\":[{\"id\":804,\"main\":\"Clouds\",\"description\":\"overcast clouds\",\"icon\":\"04n\"}],\"base\":\"stations\",\"main\":{\"temp\":283.6,\"feels_like\":277.98,\"temp_min\":283.15,\"temp_max\":284.26,\"pressure\":1010,\"humidity\":82},\"visibility\":10000,\"wind\":{\"speed\":7.2,\"deg\":210,\"gust\":12.86},\"clouds\":{\"all\":90},\"dt\":1613590036,\"sys\":{\"type\":1,\"id\":1414,\"country\":\"GB\",\"sunrise\":1613545832,\"sunset\":1613582326},\"timezone\":0,\"id\":2643743,\"name\":\"London\",\"cod\":200}";
+            main.consultWeather();
+            URL url = new URL("https://arep-clima-api.herokuapp.com/clima?lugar=bogota");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader read = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String resp = read.readLine();
+            read.close();
+            assertTrue(resp.contains("BogotÃ"));
+        } catch (Exception ex) {
+            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    /**
+     * Test of consultWeather method at the Heroku deployment for London
+     */
+    @Test
+    public void testConsultWeatherLondonHeroku() {
+        try {
+            System.out.println("consultWeather");
+            main.consultWeather();
+            URL url = new URL("https://arep-clima-api.herokuapp.com/clima?lugar=London");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader read = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String resp = read.readLine();
+            read.close();
+            assertTrue(resp.contains("London"));
+        } catch (Exception ex) {
+            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 
 }
