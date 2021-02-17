d2823a8b99acffe959a75f3742d01526574ace39
Nicolas Ortega <nicolas.ortega-l@mail.escuelaing.edu.co>
2021-02-17 14:18:17 -0500

    App funcional;

---
+++ src/main/java/edu/escuelaing/arem/clima/cache/climaCache.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem.clima.cache;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class climaCache {
+    private HashMap<String,String> cache;
+    public static climaCache _Instance=new climaCache();
+    
+    private climaCache(){
+        cache= new HashMap<String,String>();
+    }
+    public void addElement(String llave, String valor){
+        cache.put(llave,valor);
+    }
+    public String getElement(String llave){
+        return cache.get(llave);
+    }
+    public static climaCache getInstace (){
+        return _Instance;
+    }
+    public boolean contains(String e){
+        boolean res=false;
+        for (String s:cache.keySet()){
+            if(s.equals(e)){
+                res=true;
+                break;
+            }
+        }
+        return res;
+    }
+}
--- src/main/java/edu/escuelaing/arem/clima/main.java
+++ src/main/java/edu/escuelaing/arem/clima/main.java
@@ -5,7 +5,13 @@
  */
 package edu.escuelaing.arem.clima;
 
-import static spark.Spark.*;   
+import edu.escuelaing.arem.clima.cache.climaCache;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import spark.QueryParamsMap;
+import static spark.Spark.*;
 
 /**
  *
@@ -13,11 +19,59 @@ import static spark.Spark.*;
  */
 public class main {
     public static void main(String[] args) {
- 
+        port(getPort());
+        staticFileLocation("public");
+        consultWeather();
+
+        /*
         get("/clima", (req, res)->"Aqui mostraremos la implementacion del servicio del clima");
         
         get("/clima/:ciudad", (req,res)->{
             return "Aqui mostraremos la implementacion del servicio del clima, "+ req.params(":ciudad");
         });
+         */
+    }
+
+    /**
+     * @return retorna el el puerto desde el entorno del sistema operativo
+     */
+    public static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; // returns default port if heroku-port isn't set
+    }
+
+    /**
+     * Calcula la media de un str de con la lista de numeros
+     */
+    public static void consultWeather() {
+        final String key = "170e2a5df68abbdd9ad25546e7239918";
+
+        get("/clima", (req, res) -> {
+            //return "Aqui mostraremos la implementacion del servicio del clima, "+ req.params(":ciudad");
+            QueryParamsMap map = req.queryMap();
+            try {
+                String ciudad = map.get("lugar").value();
+                climaCache cache=climaCache.getInstace();
+                String resp= null;
+                if (cache.contains(ciudad)){
+                    resp = cache.getElement(ciudad);
+                }else{
+                    URL url =new URL("http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=" + key);
+                    URLConnection urlConnection = url.openConnection();
+                    BufferedReader read = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    resp = read.readLine();  
+                    cache.addElement(ciudad, resp);
+                    read.close();
+                }
+                            
+                return resp;
+            } catch (Exception e) {
+                return "Error: " + e.getMessage();
+            }
+        });
+        
+        
     }
 }
